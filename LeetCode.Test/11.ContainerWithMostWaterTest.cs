b43a8fe813f99d2f937f2df2bb23099b84ef6b9b
Indream <indreamluo@qq.com>
2015-06-25 11:31:23 +0800

    Lost Files

---
+++ LeetCode.Test/11.ContainerWithMostWaterTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Code;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class ContainerWithMostWaterTest
+    {
+        ContainerWithMostWater containerWithMostWater;
+
+        public ContainerWithMostWaterTest()
+        {
+            this.containerWithMostWater = new ContainerWithMostWater();
+        }
+
+        [TestMethod]
+        public void MaxArea()
+        {
+            var index = 0;
+            Assert.AreEqual(this.containerWithMostWater.MaxArea(new int[15000].Select(item => ++index).ToArray()), 56250000);
+            Assert.AreEqual(this.containerWithMostWater.MaxArea(new int[15000].Select(item => index--).ToArray()), 56250000);
+            Assert.AreEqual(this.containerWithMostWater.MaxArea(new int[] { 2, 4, 6, 6, 4, 2 }), 12);
+        }
+    }
+}
---
+++ LeetCode.Test/12.IntegerToRomanTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Code;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class IntegerToRomanTest
+    {
+        IntegerToRoman integerToRoman = new IntegerToRoman();
+
+        [TestMethod]
+        public void IntToRoman()
+        {
+            Assert.AreEqual(integerToRoman.IntToRoman(1), "I");
+            Assert.AreEqual(integerToRoman.IntToRoman(2), "II");
+            Assert.AreEqual(integerToRoman.IntToRoman(3), "III");
+            Assert.AreEqual(integerToRoman.IntToRoman(4), "IV");
+            Assert.AreEqual(integerToRoman.IntToRoman(5), "V");
+            Assert.AreEqual(integerToRoman.IntToRoman(6), "VI");
+            Assert.AreEqual(integerToRoman.IntToRoman(7), "VII");
+            Assert.AreEqual(integerToRoman.IntToRoman(8), "VIII");
+            Assert.AreEqual(integerToRoman.IntToRoman(9), "IX");
+            Assert.AreEqual(integerToRoman.IntToRoman(10), "X");
+            Assert.AreEqual(integerToRoman.IntToRoman(11), "XI");
+            Assert.AreEqual(integerToRoman.IntToRoman(15), "XV");
+            Assert.AreEqual(integerToRoman.IntToRoman(65), "LXV");
+            Assert.AreEqual(integerToRoman.IntToRoman(80), "LXXX");
+            Assert.AreEqual(integerToRoman.IntToRoman(999), "CMXCIX");
+            Assert.AreEqual(integerToRoman.IntToRoman(1000), "M");
+            Assert.AreEqual(integerToRoman.IntToRoman(3999), "MMMCMXCIX");
+        }
+    }
+}
