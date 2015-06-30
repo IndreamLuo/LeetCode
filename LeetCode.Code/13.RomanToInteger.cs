a446885ba0ce99cde00035c6488672555a72d4b3
Indream <indreamluo@qq.com>
2015-06-30 14:54:15 +0800

    13.RomanToInteger

--- LeetCode.Code/13.RomanToInteger.cs
+++ LeetCode.Code/13.RomanToInteger.cs
@@ -8,9 +8,41 @@ namespace LeetCode.Code
 {
     public class RomanToInteger
     {
+		static Dictionary<char, int> numberLetters = new Dictionary<char, int>
+		{
+			{ 'I', 1 },
+			{ 'V', 5 },
+			{ 'X', 10 },
+			{ 'L', 50 },
+			{ 'C', 100 },
+			{ 'D', 500 },
+			{ 'M', 1000 }
+		};
+
         public int RomanToInt(string s)
         {
-            throw new NotImplementedException();
+			var lastChar = s[0];
+			var lastCharValue = RomanToInteger.numberLetters[lastChar];
+			var tempValue = lastCharValue;
+			var result = 0;
+
+			foreach (var @char in s.Skip(1))
+			{
+				if (@char == lastCharValue)
+					tempValue += lastCharValue;
+				else
+				{
+					var currentCharValue = RomanToInteger.numberLetters[@char];
+					result += currentCharValue > lastCharValue
+						? -tempValue
+						: tempValue;
+
+					lastCharValue = currentCharValue;
+					tempValue = currentCharValue;
+				}
+			}
+
+			return result + tempValue;
         }
     }
 }
