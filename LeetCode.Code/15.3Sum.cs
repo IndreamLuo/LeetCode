182b31b81a46e0c20ac10b1693d013f8b70c21c1
Indream <indreamluo@qq.com>
2015-07-03 16:35:37 +0800

    15.3Sum

---
+++ LeetCode.Code/15.3Sum.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code
+{
+	public class _3Sum
+	{
+		public IList<IList<int>> ThreeSum(int[] nums)
+		{
+			var numbers = nums
+				.OrderBy(number => number)
+				.ToArray();
+
+			var positions = new Dictionary<int, int>();
+			for (var index = 0; index < numbers.Length; index++)
+				positions[numbers[index]] = index;
+
+			var result = new List<IList<int>>();
+			var secondEnd = numbers.Length - 1;
+			for (var secondIndex = 1; secondIndex < secondEnd; secondIndex++)
+			{
+				var secondNumber = numbers[secondIndex];
+				if (secondIndex > 1
+					&& secondNumber == numbers[secondIndex - 2])
+					continue;
+
+				for (var firstIndex =
+						secondNumber == numbers[secondIndex - 1]
+							? secondIndex - 1
+							: 0
+					; firstIndex < secondIndex
+						&& numbers[firstIndex] <= 0
+					; firstIndex++)
+				{
+					var firstNumber = numbers[firstIndex];
+					if (firstIndex > 0
+						&& firstNumber == numbers[firstIndex - 1])
+						continue;
+
+					var thirdNumber = 0 - firstNumber - secondNumber;
+
+					if (firstIndex == 0
+						&& thirdNumber < secondNumber)
+						return result;
+
+					int thirdIndex;
+					if (positions.TryGetValue(thirdNumber, out thirdIndex)
+						&& thirdIndex > secondIndex)
+						result.Add(new List<int> { firstNumber, secondNumber, thirdNumber });
+				}
+			}
+
+			return result;
+		}
+	}
+}
---
+++ LeetCode.Test/15.3SumTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using LeetCode.Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+	[TestClass]
+	public class _3SumTest
+	{
+		_3Sum _3Sum = new _3Sum();
+
+		void AssertEquals(IList<IList<int>> firstList, IList<IList<int>> secondList)
+		{
+			Assert.AreEqual(firstList.Count(), secondList.Count());
+
+			firstList = firstList
+				.OrderBy(list => list[0])
+				.ThenBy(list => list[1])
+				.ThenBy(list => list[2])
+				.ToList();
+
+			secondList = secondList
+				.OrderBy(list => list[0])
+				.ThenBy(list => list[1])
+				.ThenBy(list => list[2])
+				.ToList();
+
+			Assert.IsTrue(firstList
+				.Zip(secondList
+					, (firstSubList, secondeSubList) =>
+						firstSubList
+							.Zip(secondeSubList
+								, (first, second) =>
+									first == second)
+							.All(item => item))
+				.All(item => item)); ;
+		}
+
+		[TestMethod]
+		public void ThreeSum()
+		{
+			this.AssertEquals(_3Sum
+					.ThreeSum(new int[] { -1, 0, 1, 2, -1, -4 })
+				, new List<IList<int>>
+				{
+					new List<int> { -1, 0, 1 }
+					, new List<int> { -1, -1, 2 }
+				});
+
+			this.AssertEquals(_3Sum
+					.ThreeSum(new int[] { -1, 0, 1, 0 })
+				, new List<IList<int>>
+				{
+					new List<int> { -1, 0, 1}
+				});
+
+			this.AssertEquals(_3Sum
+					.ThreeSum(new int[] { 1, -1, -1, 0 })
+				, new List<IList<int>>
+				{
+					new List<int> { -1, 0, 1}
+				});
+
+			this.AssertEquals(_3Sum
+					.ThreeSum(new int[] { -4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6 })
+				, new List<IList<int>>
+				{
+					 new List<int> { -4,-2, 6 }
+					 , new List<int> { -4, 0, 4 }
+					 , new List<int> { -4, 1, 3 }
+					 , new List<int> { -4, 2, 2 }
+					 , new List<int> { -2, -2, 4 }
+					 , new List<int> { -2, 0, 2 }
+				});
+		}
+	}
+}
