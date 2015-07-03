23ee25de36eaf60ef9047c542158657631e2543a
Indream <indreamluo@qq.com>
2015-07-03 22:26:01 +0800

    18.4 Sum

---
+++ LeetCode.Code/18.4Sum.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code
+{
+    public class _4Sum
+    {
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            var numbers = nums
+                .OrderBy(item => item)
+                .ToArray();
+
+            var lastPositions = new Dictionary<int, int>();
+            for (var index = 0; index < numbers.Length; index++)
+                lastPositions[numbers[index]] = index;
+
+            var result = new List<IList<int>>();
+
+            var thirdIndexEnd = nums.Length - 1;
+            for (var thirdIndex = 2
+                ; thirdIndex < thirdIndexEnd
+                ; thirdIndex++)
+            {
+                var thirdNumber = numbers[thirdIndex];
+                if (thirdIndex > 2
+                    && thirdNumber == numbers[thirdIndex - 3])
+                    continue;
+
+                for (var secondIndex = 1
+                    ; secondIndex < thirdIndex
+                    ; secondIndex++)
+                {
+                    if (thirdNumber == numbers[thirdIndex - 1])
+                        secondIndex = thirdIndex - 1;
+
+                    var secondNumber = numbers[secondIndex];
+                    if (secondIndex > 1
+                        && secondNumber == numbers[secondIndex - 2])
+                        continue;
+
+                    for (var firstIndex = 0
+                        ; firstIndex < secondIndex
+                        ; firstIndex++)
+                    {
+                        if (secondNumber == numbers[secondIndex - 1])
+                            firstIndex = secondIndex - 1;
+
+                        var firstNumber = numbers[firstIndex];
+                        if (firstIndex > 0
+                            && firstNumber == numbers[firstIndex - 1])
+                            continue;
+
+                        var forthNumber = target - firstNumber - secondNumber - thirdNumber;
+                        int forthIndex;
+                        if (lastPositions.TryGetValue(forthNumber, out forthIndex)
+                            && forthIndex > thirdIndex)
+                            result.Add(new List<int> { firstNumber, secondNumber, thirdNumber, forthNumber });
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Test/18.4SumTest.cs
@@ -0,0 +1,55 @@
+﻿using LeetCode.Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class _4SumTest
+    {
+        _4Sum _4Sum = new _4Sum();
+
+        void AssertEquals(IList<IList<int>> firstList, IList<IList<int>> secondList)
+        {
+            Assert.AreEqual(firstList.Count(), secondList.Count());
+
+            firstList = firstList
+                .OrderBy(list => list[0])
+                .ThenBy(list => list[1])
+                .ThenBy(list => list[2])
+                .ThenBy(list => list[3])
+                .ToList();
+
+            secondList = secondList
+                .OrderBy(list => list[0])
+                .ThenBy(list => list[1])
+                .ThenBy(list => list[2])
+                .ThenBy(list => list[3])
+                .ToList();
+
+            Assert
+                .IsTrue(firstList
+                    .Zip(secondList
+                        , (firstSubList, secondSubList) =>
+                            firstSubList
+                                .Zip(secondSubList
+                                    , (first, second) =>
+                                        first == second)
+                                .All(item => item))
+                    .All(item => item));
+        }
+
+        [TestMethod]
+        public void FourSum()
+        {
+            this.AssertEquals(_4Sum.FourSum(new int[] { 1, 0, -1, 0, -2, 2 }, 0)
+                , new List<IList<int>>
+                {
+                    new List<int>() { -1, 0, 0, 1 }
+                    , new List<int>() { -2, -1, 1, 2 }
+                    , new List<int>() { -2, 0, 0, 2 }
+                });
+        }
+    }
+}
