9d52520f4cfc99e5b17831cfc0295370cc3323f9
Indream <indreamluo@qq.com>
2015-07-03 21:18:54 +0800

    17.Letter Combinations of a Phone Number

---
+++ LeetCode.Test/17.LetterCombinationsOfAPhoneNumberTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Code;
+using System.Collections.Generic;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class LetterCombinationsOfAPhoneNumberTest
+    {
+        LetterCombinationsOfAPhoneNumber letterCombinationsOfAPhoneNumber = new LetterCombinationsOfAPhoneNumber();
+
+        void AssertEquers(IList<string> firstList, IList<string> secondList)
+        {
+            Assert.AreEqual(firstList.Count(), secondList.Count());
+
+            firstList = firstList.OrderBy(item => item).ToList();
+            secondList = secondList.OrderBy(item => item).ToList();
+
+            Assert
+                .IsTrue(firstList
+                    .Zip(secondList
+                        , (first, second) =>
+                            first == second)
+                    .All(item => item));
+        }
+
+        [TestMethod]
+        public void LetterCombinations()
+        {
+            this.AssertEquers(letterCombinationsOfAPhoneNumber.LetterCombinations("23")
+                , new List<string> { "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" });
+
+            this.AssertEquers(letterCombinationsOfAPhoneNumber.LetterCombinations("")
+                , new List<string> { });
+        }
+    }
+}
