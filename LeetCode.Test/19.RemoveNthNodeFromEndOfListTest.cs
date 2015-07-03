17e3e8a31915593b45b65cb46b51eaab4093610b
Indream <indreamluo@qq.com>
2015-07-03 22:57:54 +0800

    19.Remove Nth Node From End of List

---
+++ LeetCode.Test/19.RemoveNthNodeFromEndOfListTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Code;
+using System.Collections.Generic;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class RemoveNthNodeFromEndOfListTest
+    {
+        RemoveNthNodeFromEndOfList removeNthNodeFromEndOfList = new RemoveNthNodeFromEndOfList();
+
+        [TestMethod]
+        public void RemoveNthFromEnd()
+        {
+            var listNodes = new List<LeetCode.Code.RemoveNthNodeFromEndOfList.ListNode>()
+            {
+                new LeetCode.Code.RemoveNthNodeFromEndOfList.ListNode(0)
+            };
+            for (var index = 1; index < 10; index++)
+            {
+                listNodes.Add(new RemoveNthNodeFromEndOfList.ListNode(index));
+                listNodes[index - 1].next = listNodes[index];
+            }
+
+            Assert.AreEqual(removeNthNodeFromEndOfList
+                .RemoveNthFromEnd(listNodes[0], 8)
+                .next
+                .next
+                .val
+                , listNodes[3]
+                .val);
+
+            Assert.AreEqual(removeNthNodeFromEndOfList
+                .RemoveNthFromEnd(listNodes[0], 8)
+                .next
+                .next
+                .val
+                , listNodes[4]
+                .val);
+
+            Assert.AreEqual(removeNthNodeFromEndOfList
+                .RemoveNthFromEnd(listNodes[0], 8)
+                .val
+                , listNodes[3]
+                .val);
+
+            Assert.AreEqual(removeNthNodeFromEndOfList
+                .RemoveNthFromEnd(listNodes[0], 1)
+                .next
+                .next
+                .next
+                .next
+                .next
+                .next
+                .val
+                , listNodes[8]
+                .val);
+        }
+    }
+}
