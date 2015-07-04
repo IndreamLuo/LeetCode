0aa0e39dd34b055ef4b149e01999786482246414
Indream <indreamluo@qq.com>
2015-07-04 14:22:17 +0800

    20.Valid Parentheses

---
+++ LeetCode.Code/20.ValidParentheses.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code
+{
+    public class ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+
+            char pairChar = ' ';
+            foreach (var @char in s)
+            {
+                switch (@char)
+                {
+                    case '(':
+                    case '[':
+                    case '{':
+                        stack.Push(@char);
+                        break;
+
+                    case ')':
+                        pairChar = '(';
+                        goto default;
+                    case ']':
+                        pairChar = '[';
+                        goto default;
+
+                    case '}':
+                        pairChar = '{';
+                        goto default;
+
+                    default:
+                        if (stack.Count == 0)
+                            return false;
+
+                        if (stack.Peek() != pairChar)
+                            return false;
+
+                        stack.Pop();
+                        break;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ LeetCode.Test/20.ValidParenthesesTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Code;
+
+namespace LeetCode.Test
+{
+    [TestClass]
+    public class ValidParenthesesTest
+    {
+        ValidParentheses validParenthese = new ValidParentheses();
+
+        [TestMethod]
+        public void IsValid()
+        {
+            Assert.IsFalse(validParenthese.IsValid("["));
+            Assert.IsTrue(validParenthese.IsValid("{[]}()"));
+            Assert.IsFalse(validParenthese.IsValid("{[}()"));
+        }
+    }
+}
