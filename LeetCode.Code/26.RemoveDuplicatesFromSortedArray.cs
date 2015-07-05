173da9eb7e9fe9187a103e2e68371d47a29099f2
Indream <indreamluo@qq.com>
2015-07-06 03:31:12 +0800

    26.Remove Duplicates from Sorted Array

---
+++ LeetCode.Code/26.RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code
+{
+    public class RemoveDuplicatesFromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            var rewriteIndex = 0;
+
+            for (var index = 1; index < nums.Length; index++)
+                if (nums[index] != nums[index - 1])
+                    nums[++rewriteIndex] = nums[index];
+
+            return nums.Length == 0
+                ? 0
+                : ++rewriteIndex;
+        }
+    }
+}
