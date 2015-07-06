bec5df2aa59c29277510039b7bd20e677b30cd15
Indream <indreamluo@qq.com>
2015-07-06 16:07:46 +0800

    27.Remove Element

---
+++ LeetCode.Code/27.RemoveElement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code
+{
+	public class RemoveElement_
+	{
+		public int RemoveElement(int[] nums, int val)
+		{
+			var replaceIndex = -1;
+
+			for (var index = 0; index < nums.Length; index++)
+				if (nums[index] != val)
+					nums[++replaceIndex] = nums[index];
+
+			return nums.Length == 0
+				? 0
+				: replaceIndex + 1;
+		}
+	}
+}
---
+++ LeetCode.Test/27.RemoveElementTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using LeetCode.Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+	[TestClass]
+	public class RemoveElementTest
+	{
+		RemoveElement_ removeElement = new RemoveElement_();
+
+		void AssertRight(int[] originArray, int length, int[] array)
+		{
+			var target = originArray
+				.Where(item => item != originArray[0])
+				.OrderBy(item => item);
+
+			var compare = array
+				.Take(length)
+				.OrderBy(item => item);
+
+			Assert.AreEqual(length, target.Count());
+
+			Assert.IsTrue(target
+				.Zip(compare
+					, (first, second) =>
+					first == second)
+				.All(item => item));
+		}
+
+		[TestMethod]
+		public void RemoveElement()
+		{
+			var arrayLength = 100;
+			var array = new int[100];
+			var random = new Random(Guid.NewGuid().GetHashCode());
+			var repeatTimes = 5;
+			for (var index = 0; index < arrayLength; index++)
+			{
+				var number = random.Next(int.MaxValue);
+				var end = index + random.Next(1, repeatTimes);
+				for (; index < arrayLength && index < end; index++)
+				{
+					array[index] = number;
+				}
+			}
+			array = array.OrderBy(item => random.Next(int.MaxValue)).ToArray();
+			var newArray = array.ToArray();
+
+			this.AssertRight(array, removeElement.RemoveElement(newArray, newArray[0]), newArray);
+		}
+	}
+}
