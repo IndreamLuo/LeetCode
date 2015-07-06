151444d6d56dac2e9d7f387403587dadd49800a2
Indream <indreamluo@qq.com>
2015-07-06 09:41:53 +0800

    13.3 Sum Closest

---
+++ LeetCode.Code/16.3SumClosest .cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code
+{
+	public class _3SumClosest
+	{
+		public int ThreeSumClosest(int[] nums, int target)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ LeetCode.Test/16.3SumClosestTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LeetCode.Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+	[TestClass]
+	public class _3SumClosestTest
+	{
+		_3SumClosest _3SumClosest = new _3SumClosest();
+
+		[TestMethod]
+		public void ThreeSumClosest()
+		{
+		}
+	}
+}
