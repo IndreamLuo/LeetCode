f19ca830983df90444cdf89e4d3e1f1debf15032
Indream <indreamluo@qq.com>
2015-07-08 14:14:55 +0800

    31.Next Permutation

---
+++ LeetCode.Code/31.NextPermutation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code
+{
+	public class NextPermutation_
+	{
+		public void NextPermutation(int[] nums)
+		{
+			var index = nums.Length - 2;
+			while (index >= 0
+				&& nums[index] >= nums[index + 1])
+				index--;
+
+			var swapIndex = nums.Length - 1;
+			while (index >= 0
+				&& swapIndex > index
+				&& nums[swapIndex] <= nums[index])
+				swapIndex--;
+
+			if (index < 0)
+				swapIndex = nums.Length / 2;
+			else
+			{
+				this.Swap(nums, index, swapIndex);
+
+				for (var bubblingIndex = swapIndex
+					; bubblingIndex < nums.Length - 1
+					&& nums[bubblingIndex] < nums[bubblingIndex + 1]
+					; bubblingIndex++)
+					this.Swap(nums, bubblingIndex, bubblingIndex + 1);
+
+				for (var bubblingIndex = swapIndex
+					; bubblingIndex > index
+					&& nums[bubblingIndex] > nums[bubblingIndex - 1]
+					; bubblingIndex++)
+					this.Swap(nums, bubblingIndex, bubblingIndex - 1);
+			}
+
+			var swapingIndex = -1;
+			int leftIndex, rightIndex;
+			while ((leftIndex
+					= index + ++swapingIndex + 1)
+				< (rightIndex
+					= nums.Length - swapingIndex - 1))
+				this.Swap(nums, leftIndex, rightIndex);
+		}
+
+		void Swap(int[] nums, int index, int swapIndex)
+		{
+			nums[index] += nums[swapIndex];
+			nums[swapIndex] = nums[index] - nums[swapIndex];
+			nums[index] -= nums[swapIndex];
+		}
+	}
+}
---
+++ LeetCode.Test/31.NextPermutationTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using LeetCode.Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+	[TestClass]
+	public class NextPermutationTest
+	{
+		NextPermutation_ nextPermutation = new NextPermutation_();
+
+		void AssertEquals(int[] subject, int[] @object)
+		{
+			Assert.AreEqual(subject.Length, @object.Length);
+			Assert.IsTrue(subject
+				.Zip(@object
+					, (first, second) => first == second)
+				.All(item => item));
+		}
+
+		[TestMethod]
+		public void NextPermutation()
+		{
+			int[] array;
+			nextPermutation.NextPermutation(array = new int[] { 1, 2, 3 });
+			this.AssertEquals(new int[] { 1, 3, 2 }, array);
+
+			nextPermutation.NextPermutation(array = new int[] { 3, 2, 1 });
+			this.AssertEquals(new int[] { 1, 2, 3 }, array);
+
+			nextPermutation.NextPermutation(array = new int[] { 1, 1, 5 });
+			this.AssertEquals(new int[] { 1, 5, 1 }, array);
+
+			nextPermutation.NextPermutation(array = new int[] { 2, 3, 1 });
+			this.AssertEquals(new int[] { 3, 1, 2 }, array);
+		}
+	}
+}
