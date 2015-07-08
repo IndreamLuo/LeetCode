589c281cde4eb1d04416141d0a037efcb326cca9
Indream <indreamluo@qq.com>
2015-07-08 18:43:59 +0800

    32.Longest Valid Parentheses(backup)

---
+++ LeetCode.Code/32.LongestValidParentheses.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code
+{
+	public class LongestValidParentheses_
+	{
+		public int LongestValidParentheses(string s)
+		{
+			var result = 0;
+
+			var lastRightParentheseIndex = -1;
+			var lastRightParentheses = new int[s.Length];
+			var startIndexes = new int[s.Length];
+
+			for (var index = 0; index < s.Length; index++)
+				if (s[index] == ')')
+				{
+					lastRightParentheses[index] = lastRightParentheseIndex;
+					lastRightParentheseIndex = index;
+
+					var currentRightParentheses = 1;
+					var currentRightParentheseIndex = index;
+					while (currentRightParentheseIndex > 0)
+					{
+						//if (currentRightParentheseIndex - lastRightParentheses[index] < currentRightParentheses)
+						throw new NotImplementedException();
+					}
+				}
+
+			return result;
+		}
+	}
+}
---
+++ LeetCode.Test/32.LongestValidParenthesesTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using LeetCode.Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+	[TestClass]
+	public class LongestValidParenthesesTest
+	{
+		LongestValidParentheses_ longestValidParenthese = new LongestValidParentheses_();
+
+		[TestMethod]
+		public void LongestValidParentheses()
+		{
+			Assert.AreEqual(4, longestValidParenthese.LongestValidParentheses(")()())"));
+			Assert.AreEqual(2, longestValidParenthese.LongestValidParentheses("(()"));
+			Assert.AreEqual(0, longestValidParenthese.LongestValidParentheses("("));
+			Assert.AreEqual(2, longestValidParenthese.LongestValidParentheses("()(()"));
+			Assert.AreEqual(8, longestValidParenthese.LongestValidParentheses("(()()(())(("));
+			Assert.AreEqual(2, longestValidParenthese.LongestValidParentheses("(((()(()"));
+		}
+	}
+}
