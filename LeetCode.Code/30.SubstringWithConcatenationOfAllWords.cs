56d069cd24245d063b0c9f777c33995bb1e9d20a
Indream <indreamluo@qq.com>
2015-07-08 09:32:46 +0800

    30.Substring with Concatenation of All Words

---
+++ LeetCode.Code/30.SubstringWithConcatenationOfAllWords.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code
+{
+	public class SubstringWithConcatenationOfAllWords
+	{
+		public IList<int> FindSubstring(string s, string[] words)
+		{
+			var result = new List<int>();
+
+			if (words.Length == 0)
+				return result;
+
+			var wordLength = words[0].Length;
+
+			var wordCount = new Dictionary<string, int>();
+			foreach (var word in words)
+				if (wordCount.ContainsKey(word))
+					wordCount[word]++;
+				else
+					wordCount[word] = 1;
+
+			var queue = new Queue<string>();
+			var concatenationLength = words.Length * wordLength;
+			for (var index = 0; index < wordLength; index++)
+			{
+				for (var subIndex = index; subIndex < s.Length - wordLength + 1; subIndex += wordLength)
+				{
+					var subString = s.Substring(subIndex, wordLength);
+					if (wordCount.ContainsKey(subString))
+					{
+						queue.Enqueue(subString);
+						wordCount[subString]--;
+
+						while (wordCount[subString] < 0)
+							wordCount[queue.Dequeue()]++;
+
+						if (queue.Count == words.Length)
+							result.Add(subIndex + wordLength - concatenationLength);
+					}
+					else
+						while (queue.Count > 0)
+							wordCount[queue.Dequeue()]++;
+				}
+
+				while (queue.Count > 0)
+					wordCount[queue.Dequeue()]++;
+			}
+
+			return result;
+		}
+	}
+}
---
+++ LeetCode.Test/30.SubstringWithConcatenationOfAllWordsTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeetCode.Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+	[TestClass]
+	public class SubstringWithConcatenationOfAllWordsTest
+	{
+		SubstringWithConcatenationOfAllWords substringWithConcatenationOfAllWords = new SubstringWithConcatenationOfAllWords();
+
+		void AssertEquals(IList<int> subject, IList<int> @object)
+		{
+			Assert.AreEqual(subject.Count, @object.Count);
+
+			Assert.IsTrue(subject
+				.Zip(@object
+					, (first, second) => first == second)
+				.All(item => item));
+		}
+
+		[TestMethod]
+		public void FindSubstring()
+		{
+			this.AssertEquals(substringWithConcatenationOfAllWords.FindSubstring("barfoothefoobarman", new string[] { "foo", "bar" })
+				, new List<int> { 0, 9 });
+			this.AssertEquals(substringWithConcatenationOfAllWords.FindSubstring("barfoothefoobarman", new string[] { })
+				, new List<int> { });
+			this.AssertEquals(substringWithConcatenationOfAllWords.FindSubstring("barfoothefoobarman", new string[] { "b" })
+				, new List<int> { 0, 12 });
+		}
+	}
+}
