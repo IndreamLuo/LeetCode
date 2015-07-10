8a1d6d4c178b8675ef805aebf3bdc514b18c3115
Indream <indreamluo@qq.com>
2015-07-10 10:03:53 +0800

    35.Search Insert Position

---
+++ LeetCode.Code/35.SearchInsertPosition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Code
+{
+	public class SearchInsertPosition
+	{
+		public int SearchInsert(int[] nums, int target)
+		{
+			var searchStartIndex = 0;
+			var searchEndIndex = nums.Length;
+
+			if (target > nums.Last())
+				return nums.Length;
+			else if (target < nums.First())
+				return 0;
+
+			while (searchEndIndex > searchStartIndex)
+			{
+				var middle = (searchStartIndex + searchEndIndex) / 2;
+
+				if (nums[middle] >= target)
+					searchEndIndex = middle;
+				else if (searchStartIndex != middle)
+					searchStartIndex = middle;
+				else
+					searchStartIndex = searchEndIndex;
+			}
+
+			return searchStartIndex;
+		}
+	}
+}
---
+++ LeetCode.Test/35.SearchInsertPositionTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LeetCode.Code;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test
+{
+	[TestClass]
+	public class SearchInsertPositionTest
+	{
+		SearchInsertPosition searchInsertPosition = new SearchInsertPosition();
+
+		[TestMethod]
+		public void SearchInsert()
+		{
+			Assert.AreEqual(2, searchInsertPosition.SearchInsert(new int[] { 1, 3, 5, 6 }, 5));
+			Assert.AreEqual(1, searchInsertPosition.SearchInsert(new int[] { 1, 3, 5, 6 }, 2));
+			Assert.AreEqual(4, searchInsertPosition.SearchInsert(new int[] { 1, 3, 5, 6 }, 7));
+			Assert.AreEqual(0, searchInsertPosition.SearchInsert(new int[] { 1, 3, 5, 6 }, 0));
+		}
+	}
+}
