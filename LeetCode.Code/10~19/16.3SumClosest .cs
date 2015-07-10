fed48965ea5936b98bfa9f8c2ee5c3281a9c7d8d
Indream <indreamluo@qq.com>
2015-07-10 09:03:54 +0800

    16.Submission Details

--- LeetCode.Code/10~19/16.3SumClosest .cs
+++ LeetCode.Code/10~19/16.3SumClosest .cs
@@ -10,7 +10,34 @@ namespace LeetCode.Code
 	{
 		public int ThreeSumClosest(int[] nums, int target)
 		{
-			throw new NotImplementedException();
+			var numbers = nums.OrderBy(number => number).ToArray();
+
+			var result = 0;
+			var closest = int.MaxValue;
+
+			for (var firstIndex = 0; firstIndex < numbers.Length - 2; firstIndex++)
+			{
+				var secondIndex = firstIndex + 1;
+				var thirdIndex = numbers.Length - 1;
+				while (secondIndex < thirdIndex)
+				{
+					int sum = numbers[firstIndex] + numbers[secondIndex] + numbers[thirdIndex];
+					int distance = Math.Abs(sum - target);
+
+					if (distance < closest)
+					{
+						closest = distance;
+						result = sum;
+					}
+
+					if (sum > target)
+						thirdIndex--;
+					else
+						secondIndex++;
+				}
+			}
+
+			return result;
 		}
 	}
 }
